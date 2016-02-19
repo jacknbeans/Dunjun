fdc7bebdc398d20608aea381f0f1e6434e28e52b
jacknbeans <jack.reggin@gmail.com>
2016-02-19 16:09:16 +0100

    Tutorial 002

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,63 @@
 #include <Dunjun/Common.hpp>
 
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+GLOBAL const int g_windowWidth  = 854;
+GLOBAL const int g_windowHeight = 480;
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello world!\n";
+	GLFWwindow* window;
 
-	std::cin.ignore();
+	if (!glfwInit())
+		return EXIT_FAILURE;
+
+	window = glfwCreateWindow(
+	    g_windowWidth, g_windowHeight, "Dunjun", nullptr, nullptr);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	bool running    = true;
+	bool fullscreen = false;
+
+	while (running)
+	{
+		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		if (glfwWindowShouldClose(window) ||
+		    glfwGetKey(window, GLFW_KEY_ESCAPE))
+			running = false;
+
+		if (glfwGetKey(window, GLFW_KEY_F11))
+		{
+			fullscreen = !fullscreen;
+
+			GLFWwindow* newWindow;
+
+			if (fullscreen)
+				newWindow = glfwCreateWindow(
+				    1920, 1080, "Dunjun", glfwGetPrimaryMonitor(), window);
+			else
+				newWindow = glfwCreateWindow(
+				    g_windowWidth, g_windowHeight, "Dunjun", nullptr, window);
+
+			glfwDestroyWindow(window);
+			window = newWindow;
+			glfwMakeContextCurrent(window);
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
